a8d2eeb933e628a519c1cf910c8c259738f00e61
Bret Pudenz <bpudenz@gmail.com>
2022-09-20 10:56:38 -0500

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+
+    <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }}</title>
+
+    <style>[x-cloak] {
+            display: none !important;
+        }</style>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    @livewireStyles
+    @livewireScripts
+    @stack('scripts')
+</head>
+
+<body class="antialiased">
+
+    @yield('content')
+
+    @livewire('notifications')
+</body>
+</html>
